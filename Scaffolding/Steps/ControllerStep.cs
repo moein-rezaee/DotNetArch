e86be69c5a620b62ccbcbf305539bd85ce6e551c
Moein Rezaee <me.rezaei1996@gmail.com>
2025-08-30 16:29:26 +0330

    fix: use escaped string for controller template

--- Scaffolding/Steps/ControllerStep.cs
+++ Scaffolding/Steps/ControllerStep.cs
@@ -11,52 +11,43 @@ public class ControllerStep : IScaffoldStep
         var apiDir = Path.Combine(basePath, startupProject, plural);
         Directory.CreateDirectory(apiDir);
         var controllerFile = Path.Combine(apiDir, $"{entity}Controller.cs");
-        var content = @"using MediatR;
-using System.Collections.Generic;
-using Microsoft.AspNetCore.Mvc;
-using {{solution}}.Application.{{entities}}.Commands.Create;
-using {{solution}}.Application.{{entities}}.Commands.Update;
-using {{solution}}.Application.{{entities}}.Commands.Delete;
-using {{solution}}.Application.{{entities}}.Queries.GetById;
-using {{solution}}.Application.{{entities}}.Queries.GetAll;
-using {{solution}}.Application.{{entities}}.Queries.GetList;
-using {{solution}}.Core.Common;
-using {{solution}}.Core.Domain.{{entities}};
-
-namespace {{startupProject}}.{{entities}};
-
-[ApiController]
-[Route("api/[controller]")]
-public class {{entity}}Controller : ControllerBase
-{
-    private readonly IMediator _mediator;
-
-    public {{entity}}Controller(IMediator mediator) => _mediator = mediator;
-
-    [HttpGet("{id}")]
-    public async Task<{{entity}}?> GetById(int id) => await _mediator.Send(new Get{{entity}}ByIdQuery(id));
-
-    [HttpGet("all")]
-    public async Task<List<{{entity}}>> GetAll() => await _mediator.Send(new Get{{entity}}AllQuery());
-
-    [HttpGet]
-    public async Task<PagedResult<{{entity}}>> GetList([FromQuery] int page = 1, [FromQuery] int pageSize = 10)
-        => await _mediator.Send(new Get{{entity}}ListQuery(page, pageSize));
-
-    [HttpPost]
-    public async Task<{{entity}}> Create([FromBody] {{entity}} entity) => await _mediator.Send(new Create{{entity}}Command(entity));
-
-    [HttpPut("{id}")]
-    public async Task Update(int id, [FromBody] {{entity}} entity)
-    {
-        entity.Id = id;
-        await _mediator.Send(new Update{{entity}}Command(entity));
-    }
-
-    [HttpDelete("{id}")]
-    public async Task Delete(int id) => await _mediator.Send(new Delete{{entity}}Command(id));
-}
-";
+        var content =
+"using MediatR\n" +
+"using System.Collections.Generic;\n" +
+"using Microsoft.AspNetCore.Mvc;\n" +
+"using {{solution}}.Application.{{entities}}.Commands.Create;\n" +
+"using {{solution}}.Application.{{entities}}.Commands.Update;\n" +
+"using {{solution}}.Application.{{entities}}.Commands.Delete;\n" +
+"using {{solution}}.Application.{{entities}}.Queries.GetById;\n" +
+"using {{solution}}.Application.{{entities}}.Queries.GetAll;\n" +
+"using {{solution}}.Application.{{entities}}.Queries.GetList;\n" +
+"using {{solution}}.Core.Common;\n" +
+"using {{solution}}.Core.Domain.{{entities}};\n\n" +
+"namespace {{startupProject}}.{{entities}};\n\n" +
+"[ApiController]\n" +
+"[Route(\"api/[controller]\")]\n" +
+"public class {{entity}}Controller : ControllerBase\n" +
+"{\n" +
+"    private readonly IMediator _mediator;\n\n" +
+"    public {{entity}}Controller(IMediator mediator) => _mediator = mediator;\n\n" +
+"    [HttpGet(\"{id}\")]\n" +
+"    public async Task<{{entity}}?> GetById(int id) => await _mediator.Send(new Get{{entity}}ByIdQuery(id));\n\n" +
+"    [HttpGet(\"all\")]\n" +
+"    public async Task<List<{{entity}}>> GetAll() => await _mediator.Send(new Get{{entity}}AllQuery());\n\n" +
+"    [HttpGet]\n" +
+"    public async Task<PagedResult<{{entity}}>> GetList([FromQuery] int page = 1, [FromQuery] int pageSize = 10)\n" +
+"        => await _mediator.Send(new Get{{entity}}ListQuery(page, pageSize));\n\n" +
+"    [HttpPost]\n" +
+"    public async Task<{{entity}}> Create([FromBody] {{entity}} entity) => await _mediator.Send(new Create{{entity}}Command(entity));\n\n" +
+"    [HttpPut(\"{id}\")]\n" +
+"    public async Task Update(int id, [FromBody] {{entity}} entity)\n" +
+"    {\n" +
+"        entity.Id = id;\n" +
+"        await _mediator.Send(new Update{{entity}}Command(entity));\n" +
+"    }\n\n" +
+"    [HttpDelete(\"{id}\")]\n" +
+"    public async Task Delete(int id) => await _mediator.Send(new Delete{{entity}}Command(id));\n" +
+"}\n";
         content = content
             .Replace("{{solution}}", solution)
             .Replace("{{entity}}", entity)
