47766fdbd742e76d8de0ae3b0f67d3eeced87a55
Moein Rezaee <me.rezaei1996@gmail.com>
2025-09-02 00:45:03 +0330

    fix: remove unused configuration using

--- ServiceScaffolder.cs
+++ ServiceScaffolder.cs
@@ -3,7 +3,6 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using DotNetArch.Scaffolding;
-using Microsoft.Extensions.Configuration;
 
 static class ServiceScaffolder
 {
